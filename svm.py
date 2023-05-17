abf7f8820bdbf0173537142f0793aaf9572c5c0f
FennaBlom <90144787+FennaBlom@users.noreply.github.com>
2023-05-17 14:19:06 +0200

    Cleaning svm 17-05-2023

--- svm.py
+++ svm.py
@@ -21,6 +21,8 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import GridSearchCV
+
 
 url_train = 'https://raw.githubusercontent.com/sebischair/Medical-Abstracts-TC-Corpus/main/medical_tc_train.csv'
 df_train = pd.read_csv(url_train,index_col=0,parse_dates=[0],on_bad_lines='skip')
@@ -89,6 +91,7 @@ for index,entry in enumerate(df_test['medical_abstract']):
     # The final processed set of words for each iteration will be stored in 'text_final'
     df_test.loc[index,'text_final'] = str(Final_words)
 
+# Encode and vectorize data
 Encoder = LabelEncoder()
 Train_Y = Encoder.fit_transform(df_train['condition_label'])
 Test_Y = Encoder.fit_transform(df_test['condition_label'])
@@ -98,32 +101,17 @@ Tfidf_vect.fit(df_train['text_final'])
 Train_X_Tfidf = Tfidf_vect.transform(df_train['text_final'])
 Test_X_Tfidf = Tfidf_vect.transform(df_test['text_final'])
 
-from sklearn.model_selection import GridSearchCV
 
+# Grid search for hyperparameter optimization 
 param_grid = {'C': [0.1,1, 10], 'gamma': [1,0.1,0.01],'kernel': ['poly', 'sigmoid', 'rbf']}
 # grid = GridSearchCV(svm.SVC(),param_grid,refit=True,verbose=2)
 # grid.fit(Train_X_Tfidf,Train_Y)
 # print(grid.best_estimator_)
 
+# Fit best model
 SVM = svm.SVC(C=1.0, kernel='sigmoid', degree=3, gamma=1)
 SVM.fit(Train_X_Tfidf,Train_Y)
 # predict the labels on validation dataset
 predictions_SVM = SVM.predict(Test_X_Tfidf)
 # Use accuracy_score function to get the accuracy
 print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
-
-SVM_balance = svm.SVC(C=1.0, kernel='sigmoid', gamma=1, class_weight='balanced')
-SVM_balance.fit(Train_X_Tfidf, Train_Y)
-predictions_SVM_balance = SVM_balance.predict(Test_X_Tfidf)
-print("SVM Accuracy Score Balanced -> ",accuracy_score(predictions_SVM_balance, Test_Y)*100)
-
-# from imblearn.over_sampling import SMOTE
-# smote = SMOTE(random_state=17)
-# X_1, y_1 = smote.fit_resample(pd.DataFrame(df_train['medical_abstract']), df_train.condition_label)
-
-# Tfidf_vect_1 = TfidfVectorizer(max_features=5000)
-# Tfidf_vect_1.fit(df_train['text_final'])
-# Train_X_Tfidf = Tfidf_vect.transform(df_train['text_final'])
-# Test_X_Tfidf = Tfidf_vect.transform(df_test['text_final'])
-
-# print(classification_report(Test_Y,predictions_SVM))
